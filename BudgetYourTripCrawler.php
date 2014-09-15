654e9fc61d67b6c32f8754505dd594b66152c13f
Anthony Bieber <anthonyb@nationalfibre.net>
2014-09-15 04:57:29 -0400

    Comments added

--- BudgetYourTripCrawler.php
+++ BudgetYourTripCrawler.php
@@ -1,8 +1,10 @@
 <?php
 
+//Only used for testing purposes
 ini_set('max_execution_time', 300);
-ini_set('memory_limit','500M');
+ini_set('memory_limit','300M');
 
+//Autoload files will be merged so both do not have to be included
 require __DIR__ . '/vendor/autoload.php';
 require 'autoload.php';
 
@@ -14,16 +16,18 @@ $pageHandler = new PageHandler();
 $parser      = new BudgetYourTripParser();
 $translate   = new Translations();
 
+//Get The country list page and parse the countries from it
 $content      = $pageHandler->getPage('http://www.budgetyourtrip.com/countrylist.php');
 $countryList  = $parser     ->filterCountryList($content);
 
+//Iterate through each country, translate country name to country code, and get page to parse price
 foreach($countryList as $country) {
     $country              = trim($country);
     $countryCode          = $translate->getCountryCodeFromCountry($country);
 
+    //This process is for the budget price only, the same would be repeated for mid range and luxury
     $countryBudgetContent = $pageHandler->getPage("http://www.budgetyourtrip.com/budgetreportadv.php?geonameid=&countrysearch=&country_code=$countryCode&categoryid=0&budgettype=1&triptype=0&startdate=&enddate=&travelerno=0");
     $price                = $parser->filterPrice($countryBudgetContent);
-
     $parser->savePrice($country, 'Budget', $price);
 }
 
